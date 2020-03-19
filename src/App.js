5d73ceede03739544a6521e7a5a78385cf1c599f
sherwinvarkiani <s.varkiani@gmail.com>
2020-03-19 18:59:10 -0400

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* Render the user uploaded images */
 function Image1Render(props) {
   if (props.selectedImg1 != null) {
-    return <img src={URL.createObjectURL(props.selectedImg1)} height="300" width="400"/>
+    return <img src={URL.createObjectURL(props.selectedImg1)} class="img-fluid img-max-size m-5 border border-dark"/>
   }
-  return null
+    return <img src={require("./placeholder-image.png")} class="img-fluid img-max-size m-5 border border-dark"/>
 }
 
 function Image2Render(props) {
   if (props.selectedImg2 != null) {
-    return <img src={URL.createObjectURL(props.selectedImg2)} height="300" width="400"/>
+    return <img src={URL.createObjectURL(props.selectedImg2)} class="img-fluid img-max-size m-5 border border-dark"/>
   }
-  return null
+  return <img src={require("./placeholder-image.png")} class="img-fluid img-max-size m-5 border border-dark"/>
 }
 
 class App extends Component {
@@ -105,20 +104,40 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="imageDiv" id="left">
-          <input type="file" onChange={this.fileSelectedHandlerImg1}/>
-          <button onClick={this.fileUploadHandlerImg1}>Upload the original image</button>
-          <Image1Render selectedImg1 = {this.state.selectedImg1}/>
+      <div className="App" class="bg-light container-fluid">
+        <div class="row">
+          <div class="col bg-dark mb-3 header">
+            <h1 class="vcenter" id="title">Text Compare</h1>
+            <h2 class="vcenter" id="subtitle">By Sherwin Varkiani</h2>
+          </div>
         </div>
-        <div className="imageDiv">
-          <input type="file" onChange={this.fileSelectedHandlerImg2}/>
-          <button onClick={this.fileUploadHandlerImg2}>Upload the comparison image</button>
-          <Image2Render selectedImg2 = {this.state.selectedImg2}/>
+        <div class="row">
+          <div class="col text-center">
+            {this.state.comparedImages ? (<img src={require("./comparison/comparison.jpg")} class="img-fluid img-max-size m-5 border border-dark"/>) : null}
+          </div>
         </div>
-        <div>
-          <button onClick={this.compareImagesHandler} disabled={this.state.uploadedImg1 === false || this.state.uploadedImg2 === false}>Compare Images</button>
-          {this.state.comparedImages ? (<img src={require("./comparison.jpg")} height="300" width="400"/>) : null}
+        <div class="row">
+          <div class="col text-center">
+            <div class="input-block">
+            <input type="file" style={{display: 'none'}} onChange={this.fileSelectedHandlerImg1} ref={fileInput1 => this.fileInput1 = fileInput1}/>
+              <button class="btn btn-dark mr-5" onClick={() => this.fileInput1.click()}>Choose a file</button>
+              <button class="btn btn-dark" onClick={this.fileUploadHandlerImg1}>Upload the original image</button>
+              <Image1Render selectedImg1 = {this.state.selectedImg1}/>
+            </div>
+          </div>
+          <div class="col text-center">
+            <div class="input-block">
+              <input type="file" style={{display: 'none'}} onChange={this.fileSelectedHandlerImg2} ref={fileInput2 => this.fileInput2 = fileInput2}/>
+              <button class="btn btn-dark mr-5" onClick={() => this.fileInput2.click()}>Choose a file</button>
+              <button class="btn btn-dark" onClick={this.fileUploadHandlerImg2}>Upload the comparison image</button>
+              <Image2Render selectedImg2 = {this.state.selectedImg2}/>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col text-center">
+            <button class="btn btn-dark" onClick={this.compareImagesHandler} disabled={this.state.uploadedImg1 === false || this.state.uploadedImg2 === false}>Compare Images</button>
+          </div>
         </div>
       </div>
     );
